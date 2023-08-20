9f238a14fbb6eb32ae5b22500035c09624fefaaf
fernando.matheus <fernando.matheuss@hotmail.com>
2023-08-20 07:32:25 -0300

    Implementação de user-service.

---
+++ user-service/src/main/java/com/fmatheus/app/model/entity/User.java
@@ -0,0 +1,45 @@
+package com.fmatheus.app.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id_person"}),
+        @UniqueConstraint(columnNames = {"id"}),
+        @UniqueConstraint(columnNames = {"username"})})
+public class User extends Base {
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 70)
+    private String password;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @ToString.Exclude
+    @JoinColumn(name = "id_person", referencedColumnName = "id", nullable = false)
+    @OneToOne(optional = false)
+    private Person person;
+
+    @ToString.Exclude
+    @JoinTable(name = "user_permission_join", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_permission"))
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Permission> permissions;
+
+
+}
